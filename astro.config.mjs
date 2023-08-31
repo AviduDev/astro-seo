4c85a5ab452b6671a17da74002253b81fcd1e52e
AviduDev <avidudev@gmail.com>
2023-08-31 07:55:33 +0530

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://dazzling-souffle-ca3e6c.netlify.app',
+  integrations: [sitemap()],
+});
